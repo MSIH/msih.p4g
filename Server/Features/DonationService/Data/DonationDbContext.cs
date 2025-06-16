fd45c9c7a56f8ad554182435ebfddd3f91933827
MSIH LLC <MSIH@users.noreply.github.com>
2025-06-16 17:32:34 -0400

    Donation navigation (#49)
    * updated to make more flexiable
    
    * referral code doen
    
    * add navigaton to donation model
    
    ---------
    
    Co-authored-by: Barry Schneider (MSIH) <bschneider@msih.com>

--- Server/Features/DonationService/Data/DonationDbContext.cs
+++ Server/Features/DonationService/Data/DonationDbContext.cs
@@ -26,7 +26,23 @@ namespace msih.p4g.Server.Features.DonationService.Data
                 entity.Property(e => e.Amount).IsRequired().HasPrecision(18, 2);
                 entity.Property(e => e.DonationMessage).HasMaxLength(1000);
                 entity.Property(e => e.ReferralCode).HasMaxLength(100);
-                // Add additional configuration as needed
+                entity.Property(e => e.CampaignCode).HasMaxLength(100);
+                
+                // Configure relationships with two-way navigations
+                entity.HasOne(d => d.Donor)
+                      .WithMany(donor => donor.Donations)  // Two-way navigation
+                      .HasForeignKey(d => d.DonorId)
+                      .OnDelete(DeleteBehavior.Restrict);  // Don't cascade delete donors
+                
+                entity.HasOne(d => d.PaymentTransaction)
+                      .WithMany()  // A payment transaction may be associated with multiple donations
+                      .HasForeignKey(d => d.PaymentTransactionId)
+                      .OnDelete(DeleteBehavior.Restrict);  // Don't cascade delete payment transactions
+                
+                entity.HasOne(d => d.Campaign)
+                      .WithMany(campaign => campaign.Donations)  // Two-way navigation
+                      .HasForeignKey(d => d.CampaignId)
+                      .OnDelete(DeleteBehavior.Restrict);  // Don't cascade delete campaigns
             });
         }
     }
