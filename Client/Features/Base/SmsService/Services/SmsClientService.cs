03d6c3818ad2e72b98abeed8535a71d5ae25acd0
Barry Schneider (MSIH) <bschneider@msih.com>
2025-06-17 08:02:14 -0400

    all menu links are working

---
+++ Client/Features/Base/SmsService/Services/SmsClientService.cs
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2025 MSIH LLC. All rights reserved.
+ * This file is developed for Make Sure It Happens Inc.
+ * Unauthorized copying, modification, distribution, or use is prohibited.
+ */
+
+// This file has been intentionally left empty as it's not needed in a Blazor Server application.
+// Blazor Server components can directly inject server-side services.
