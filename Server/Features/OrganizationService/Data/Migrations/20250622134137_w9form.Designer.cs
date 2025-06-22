05e1517cee29236570671fb955f086b6e45848f4
Barry Schneider (MSIH) <bschneider@msih.com>
2025-06-22 09:52:33 -0400

    before removecallback events

--- Server/Features/OrganizationService/Data/Migrations/20250622134137_w9form.Designer.cs
+++ Server/Features/OrganizationService/Data/Migrations/20250622134137_w9form.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using msih.p4g.Server.Common.Data;
 
 #nullable disable
 
-namespace msih.p4g.Server.Common.Data.Migrations
+namespace msih.p4g.Server.Features.OrganizationService.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250622134137_w9form")]
+    partial class w9form
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
---
+++ Server/Features/OrganizationService/Data/Migrations/20250622134137_w9form.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace msih.p4g.Server.Features.OrganizationService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class w9form : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "W9Forms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    BusinessName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    FederalTaxClassification = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    LLCTaxClassification = table.Column<string>(type: "TEXT", maxLength: 1, nullable: true),
+                    OtherClassificationInstructions = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    PartnershipTrustInfo = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    ExemptPayeeCode = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true),
+                    FATCAExemptionCode = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    CityStateZip = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    AccountNumbers = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    SocialSecurityNumber = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    EmployerIdentificationNumber = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    SignedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SignatureVerification = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    FundraiserId = table.Column<int>(type: "INTEGER", nullable: true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_W9Forms", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_W9Forms_FundraiserId",
+                table: "W9Forms",
+                column: "FundraiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_W9Forms_UserId",
+                table: "W9Forms",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "W9Forms");
+        }
+    }
+}
