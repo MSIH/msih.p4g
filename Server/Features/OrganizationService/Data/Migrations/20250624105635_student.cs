83c0cb0c4910e7a4ea4afd531fe2c88bdf161fce
MSIH LLC <MSIH@users.noreply.github.com>
2025-06-24 07:31:41 -0400

    91 student page (#94)
    * base
    
    * add ap settings
    
    * more improvments
    
    * add /give as url
    
    ---------
    
    Co-authored-by: Barry Schneider <bschneider@msih.com>

---
+++ Server/Features/OrganizationService/Data/Migrations/20250624105635_student.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace msih.p4g.Server.Features.OrganizationService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class student : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
