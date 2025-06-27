64c413616b566a884be9b12006733408ee09032f
Barry Schneider (MSIH) <bschneider@msih.com>
2025-06-27 07:32:26 -0400

    add footer with env and build

--- Server/Features/Base/SettingsService/Interfaces/ISettingsService.cs
+++ Server/Features/Base/SettingsService/Interfaces/ISettingsService.cs
@@ -1,11 +1,17 @@
+// /**
+//  * Copyright (c) 2025 MSIH LLC. All rights reserved.
+//  * This file is developed for Make Sure It Happens Inc.
+//  * Unauthorized copying, modification, distribution, or use is prohibited.
+//  */
+
+
+using static msih.p4g.Server.Features.Base.SettingsService.Services.SettingsService;
+
 /**
  * Copyright (c) 2025 MSIH LLC. All rights reserved.
  * This file is developed for Make Sure It Happens Inc.
  * Unauthorized copying, modification, distribution, or use is prohibited.
  */
-using System.Threading.Tasks;
-using System.Collections.Generic;
-
 namespace msih.p4g.Server.Features.Base.SettingsService.Interfaces
 {
     /// <summary>
@@ -17,15 +23,22 @@ namespace msih.p4g.Server.Features.Base.SettingsService.Interfaces
         /// Gets a setting value by key, checking DB, then appsettings, then environment variables.
         /// </summary>
         Task<string?> GetValueAsync(string key);
-        
+
         /// <summary>
         /// Sets or updates a setting value in the DB.
         /// </summary>
         Task SetValueAsync(string key, string? value, string modifiedBy = "System");
-        
+
         /// <summary>
         /// Gets all settings from the DB as a dictionary.
         /// </summary>
         Task<IReadOnlyDictionary<string, string?>> GetAllAsync();
+
+        /// <summary>
+        /// CleanSettingsAsync
+        ///
+        /// Cleans up settings by removing any that are not in the current configuration.
+        /// </summary>
+        Task<SettingsCleanupResult> CleanSettingsAsync();
     }
 }
