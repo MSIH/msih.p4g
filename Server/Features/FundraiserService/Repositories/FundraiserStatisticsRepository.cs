ade9166787193d8a7ac90adcb7eb937c461a2d0f
MSIH LLC <MSIH@users.noreply.github.com>
2025-07-09 17:28:44 -0400

    added my fundraiser (#247)
    * added my fundraiser
    
    * cleanup
    
    * more typo
    
    ---------
    
    Co-authored-by: Barry Schneider (MSIH) <bschneider@msih.com>

--- Server/Features/FundraiserService/Repositories/FundraiserStatisticsRepository.cs
+++ Server/Features/FundraiserService/Repositories/FundraiserStatisticsRepository.cs
@@ -113,13 +113,10 @@ namespace msih.p4g.Server.Features.FundraiserService.Repositories
             var fundraiserReferralCode = fundraiser.User.Profile.ReferralCode;
 
             // Get all donors with the fundraiser's referral code who have confirmed their email or made a donation
-
-
             var donorsFromReferral = await context.Donors
                 .Where(d => d.ReferralCode == fundraiserReferralCode)
                 .Where(d => d.User.EmailConfirmedAt != null || d.Donations.Any())
                 .ToListAsync();
-
 
             return donorsFromReferral.Select(ftd => new FirstTimeDonorInfo
             {
